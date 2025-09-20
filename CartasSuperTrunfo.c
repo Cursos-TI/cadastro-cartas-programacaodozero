9da42765ae1214b60e6e2d6dc90d78df331c31f5
programacaodozero <programgithub27@gmail.com>
2025-09-20 18:41:07 +0000

    cadastro carta

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -6,8 +6,32 @@
 
 int main() {
   // Área para definição das variáveis para armazenar as propriedades das cidades
+  int populacao1, pontos_turisticos1;
+  float area1, pib1;
+  char estado1, codigo1, cidade1;
 
   // Área para entrada de dados
+  // Carta 1
+  printf("Estado: \n");
+  scanf("%s", &estado1);
+
+  printf("Código: \n");
+  scanf("%s", &codigo1);
+
+  printf("Cidade: \n");
+  scanf("%s", &cidade1);
+
+  printf("População: \n");
+  scanf("%d", &populacao1);
+
+  printf("Área: \n");
+  scanf("%f", &area1);
+
+  printf("PIB: \n");
+  scanf("%f", &pib1);
+
+  printf("Nº de Pontos Turísticos: \n");
+  scanf("%d", &pontos_turisticos1);
 
   // Área para exibição dos dados da cidade
 
